341394c3c639c36947b1cfd22777a512e5469473
Antonio Almeida <promatik@gmail.com>
2022-11-18 00:00:46 +0000

    Make sure view names use snake case

--- src/Console/Commands/PageBackpackCommand.php
+++ src/Console/Commands/PageBackpackCommand.php
@@ -59,10 +59,11 @@ class PageBackpackCommand extends GeneratorCommand
             ->trim('/');
 
         $name = $input->afterLast('/');
-        $nameSnake = $name->snake();
-        $nameTitle = $nameSnake->replace('_', ' ')->replace('-', ' ')->title();
+        $nameTitle = $name->snake()->replace('-', ' ')->replace('_', ' ')->title();
+        $nameSnake = $nameTitle->snake();
+
         $path = $input->beforeLast($name)->trim('/\\');
-        $filePath = Str::of("$path/$name")->trim('/\\');
+        $filePath = Str::of("$path/$nameSnake")->trim('/\\');
         $fullPath = $this->getPath($filePath);
         $layout = Str::of($this->option('layout'))->replace('\\', '/')->replace('/', '.');
         $route = Str::of($this->option('route') ?? $nameSnake)->replace('\\', '/')->trim('/');
